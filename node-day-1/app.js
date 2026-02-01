25aed718725fb5f80f308bf6aca16f408cb53e66
M Ahsan Sandhu <ahsanhd@outlook.com>
2026-02-01 23:21:41 +0500

    Synced Repo with work Laptop

--- node-day-1/app.js
+++ node-day-1/app.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const loggerMiddleware = require('./middleware/logger.middleware');
-const helloRouter = require('./routes/hell.routes');
-const sumRouter = require('./routes/sum.routes');
-const errorMiddleware = require('./middleware/error.middleware');
-
+const express = require("express");
+const loggerMiddleware = require("./middleware/logger.middleware");
+const helloRouter = require("./routes/hell.routes");
+const sumRouter = require("./routes/sum.routes");
+const errorMiddleware = require("./middleware/error.middleware");
 
 const app = express();
- const port = 3000;
+const port = 3000;
 
 app.use(express.json());
 app.use(loggerMiddleware);
-app.use(errorMiddleware)
-app.use('/', sumRouter);
-app.use('/', helloRouter)
+app.use(errorMiddleware);
+app.use("/", sumRouter);
+app.use("/", helloRouter);
 
+app.use(errorMiddleware);
 
+/* linked with my work Laptop */
 
-
-
-app.use(errorMiddleware)
-
-module.exports = {app, port};
+module.exports = { app, port };
